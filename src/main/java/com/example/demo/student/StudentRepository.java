7813a24ae7e6d01487af852bec05ef27c8d26b18
swtt-pch <igor.gsouza@bandtec.com.br>
2022-10-08 22:36:41 -0300

    feat: Writing Some Business Logic :sparkles:

--- src/main/java/com/example/demo/student/StudentRepository.java
+++ src/main/java/com/example/demo/student/StudentRepository.java
@@ -1,9 +1,16 @@
 package com.example.demo.student;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface StudentRepository  extends JpaRepository<Student, Long> {
+
+    // SELECT * FROM WHERE email = ?
+    //@Query("SELECT s FROM Student s WHERE s.emai = ?1")
+    Optional<Student> findStudentByEmail(String email);
 
 }
