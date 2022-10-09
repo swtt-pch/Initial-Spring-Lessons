476df70754a2ec49f6c964fcccbe015439cc74a6
swtt-pch <igor.gsouza@bandtec.com.br>
2022-10-08 22:02:37 -0300

    feat: JPA Repository :sparkles:

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,6 +1,6 @@
 package com.example.demo.student;
 
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -9,17 +9,14 @@ import java.util.List;
 
 @Service
 public class StudentService {
+    private final StudentRepository studentRepository;
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
 
-    
+
     public List<Student> getStudents(){
-        return List.of(
-                new Student(
-                        1L,
-                        "Mariam",
-                        21,
-                        LocalDate.of(2000, Month.NOVEMBER, 12),
-                        "mariam.mari@mariam.com"
-                )
-        );
+        return studentRepository.findAll();
     }
 }
